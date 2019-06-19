9e193fd18e9867d0a94f32ecd3b1a5b8caa629b3
Naimor <romain.bellande@orange.fr>
2019-06-19 02:34:02 +0200

    feat(processor): upgrade

--- client/src/components/Planet/Planet.js
+++ client/src/components/Planet/Planet.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { shape, func, arrayOf } from 'prop-types';
 
-import Resources from './Resources';
 import items from '../../common/mocks/items';
 import Processors from './Processors';
 
@@ -17,7 +16,8 @@ const Planet = ({ match, fetchPlanet, production, inventory, planet, processors 
         const resourceItem = items.find(item => item.id === parseInt(key, 10));
         const resourceProduction = production[key];
         const { id, name } = resourceItem;
-        const { actual_rate: netRate, producing_rate: producingRate } = resourceProduction;
+        const netRate = resourceProduction ? resourceProduction.actual_rate : 0;
+        const rate = resourceProduction ? resourceProduction.producing_rate : 0;
         const value = inventory[key];
 
         return {
@@ -25,7 +25,7 @@ const Planet = ({ match, fetchPlanet, production, inventory, planet, processors 
           name,
           value,
           rate: parseFloat(netRate),
-          netRate: parseFloat(producingRate),
+          netRate: parseFloat(rate),
         };
       });
 
